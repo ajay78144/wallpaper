f82de6fa1abc3fe57c6db7b0282b896699ad17a4
Ajay kumar <kajay77598@gmail.com>
2026-02-23 18:41:09 +0530

    add

---
+++ wallmagin/src/app/wallpapers/wallpapers-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WallpapersRoutingModule } from './wallpapers-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WallpapersRoutingModule
+  ]
+})
+export class WallpapersModule { }
